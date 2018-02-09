b44496ef0b8963172467814a96704b58c62eb907
RTXUX <q1034908455@gmail.com>
2018-02-09 22:36:19 +0800

    docs: Add some comments

--- Scheduler.cpp
+++ Scheduler.cpp
@@ -11,6 +11,7 @@ int Scheduler::decide() {
     auto triggeredPassengers = this->world.getTriggeredPassengers();
     int currentFloor = world.getElevator()->getCurrentFloor();
     auto insidePassengers = world.getElevator()->getInsidePassengers();
+    //处理已触发请求的入厢
     for (auto iter = triggeredPassengers.begin(); iter != triggeredPassengers.end(); iter++) {
         if ((*iter).getInitialFloor() == currentFloor) {
             if ((*iter).getDestinstaion() == 10 && (lastDecision == 1 || lastDecision == 0)) {
@@ -23,9 +24,11 @@ int Scheduler::decide() {
             }
         }
     }
+    //需要入厢
     if (pending == 1) {
         return 0;
     }
+    //刚到达边界
     if (((currentFloor == 1) || (currentFloor == 10)) && lastDecision != 0) {
         for (auto iter = insidePassengers.begin(); iter != insidePassengers.end(); iter++) {
             if ((*iter).getDestinstaion() == currentFloor) {
@@ -34,6 +37,7 @@ int Scheduler::decide() {
             }
         }
     }
+    //已在边界停留
     if (lastDecision == 0) {
         if (triggeredPassengers.size() == 0 && world.getElevator()->getInsidePassengers().size() == 0) {
             return 0;
