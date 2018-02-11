7c2839dccc8b9aa63ae35360009ac0d17e3ec5dd
RTXUX <q1034908455@gmail.com>
2018-02-11 14:39:17 +0800

    feat: Adapt in Stolf's Algorithm of Plan B

--- Scheduler.cpp
+++ Scheduler.cpp
@@ -2,56 +2,146 @@
 // Created by rtxux on 2018/2/9.
 //
 
+#include <cstdlib>
+
+using namespace std;
 #include "Scheduler.h"
+
+enum {
+    UP, DOWN, STOP
+};
 
 Scheduler::Scheduler(World &world) : world(world) {}
 
 int Scheduler::decide() {
-    int pending = 0;
-    auto triggeredPassengers = this->world.getTriggeredPassengers();
     int currentFloor = world.getElevator()->getCurrentFloor();
     auto insidePassengers = world.getElevator()->getInsidePassengers();
-    //处理已触发请求的入厢
+    auto triggeredPassengers = world.getTriggeredPassengers();
+    int destNum[3]{0}, destTime[3]{0};
+    int upFloor = 0, downFloor = 11, upExtra = 0, downExtra = 0, stopCount = 0;
+    for (auto iter = insidePassengers.begin(); iter != insidePassengers.end(); iter++) {
+        int destination = iter.base()->getDestinstaion();
+        upFloor = max(upFloor, destination);
+        downFloor = min(downFloor, destination);
+        if (destination > currentFloor) {
+            destNum[UP]++;
+        } else if (destination < currentFloor) {
+            destNum[DOWN]++;
+        } else {
+            destNum[STOP]++;
+        }
+    }
+
     for (auto iter = triggeredPassengers.begin(); iter != triggeredPassengers.end(); iter++) {
-        if ((*iter).getInitialFloor() == currentFloor) {
-            if ((*iter).getDestinstaion() == 10 && (direction == 1 || direction == 0)) {
-                pending = 1;
-                break;
+        int initialFloor = iter.base()->getInitialFloor();
+        upFloor = max(upFloor, initialFloor);
+        downFloor = min(downFloor, initialFloor);
+        if (initialFloor > currentFloor) {
+            destNum[UP]++;
+        } else if (initialFloor < currentFloor) {
+            destNum[DOWN]++;
+        } else {
+            destNum[STOP]++;
+        }
+    }
+
+    for (auto iter = insidePassengers.begin(); iter != insidePassengers.end(); iter++) {
+        int destination = iter.base()->getDestinstaion();
+        if (destNum[UP]) {
+            destTime[UP] += abs(upFloor - currentFloor) + abs(upFloor - destination);
+            if (destination != upFloor) {
+                destTime[UP]++;
             }
-            if ((*iter).getDestinstaion() == 1 && (direction == -1 || direction == 0)) {
-                pending = 1;
-                break;
+        }
+
+        if (destNum[DOWN]) {
+            destTime[DOWN] += abs(downFloor - currentFloor) + abs(downFloor - destination);
+            if (destination != downFloor) {
+                destTime[DOWN]++;
+            }
+        }
+
+        if (destNum[STOP]) {
+            if (destination != currentFloor) {
+                destTime[STOP]++;
+            } else {
+                stopCount++;
             }
         }
     }
-    //需要入厢
-    if (pending == 1) {
-        return 0;
+
+    for (auto iter = triggeredPassengers.begin(); iter != triggeredPassengers.end(); iter++) {
+        int initialFloor = iter.base()->getInitialFloor();
+        int destination = iter.base()->getDestinstaion();
+        if (destNum[UP]) {
+            destTime[UP] += abs(upFloor - currentFloor) + abs(upFloor - initialFloor) + abs(destination - initialFloor);
+            if (destination != upFloor) {
+                destTime[UP]++;
+            }
+        }
+
+        if (destNum[DOWN]) {
+            destTime[DOWN] +=
+                    abs(downFloor - currentFloor) + abs(downFloor - initialFloor) + abs(destination - initialFloor);
+            if (destination != downFloor) {
+                destTime[DOWN]++;
+            }
+        }
+
+        if (destNum[STOP]) {
+            if (initialFloor == currentFloor) {
+                stopCount++;
+                if (destination == 1) {
+                    upExtra = currentFloor - destination;
+                } else if (destination == 10) {
+                    downExtra = destination - currentFloor;
+                }
+            }
+
+        }
+
     }
-    //刚到达边界
-    if (((currentFloor == 1) || (currentFloor == 10)) && direction != 0) {
-        for (auto iter = insidePassengers.begin(); iter != insidePassengers.end(); iter++) {
-            if ((*iter).getDestinstaion() == currentFloor) {
-                direction = 0;
+    if (!destNum[UP] && !destNum[DOWN] && !destNum[STOP]) {
+        return 0;
+    } else if (!destNum[UP] && !destNum[DOWN]) {
+        return 0;
+    } else if (!destNum[STOP] && !destNum[DOWN]) {
+        return 1;
+    } else if (!destNum[UP] && !destNum[STOP]) {
+        return -1;
+    } else if (!destNum[UP]) {
+        if (destTime[STOP] * (downExtra + 1) > stopCount * ((abs(downFloor - currentFloor)) * 2 + 1)) {
+            return -1;
+        } else {
+            return 0;
+        }
+    } else if (!destNum[DOWN]) {
+        if (destTime[STOP] * (upExtra + 1) > stopCount * ((abs(upFloor - currentFloor)) * 2 + 1)) {
+            return 1;
+        } else {
+            return 0;
+        }
+    } else if (!destNum[STOP]) {
+        if (destTime[UP] < destTime[DOWN]) {
+            return 1;
+        } else {
+            return -1;
+        }
+    } else {
+        if (destTime[UP] < destTime[DOWN]) {
+            if (destTime[STOP] * (upExtra + 1) > stopCount * ((abs((int) upFloor - (int) currentFloor)) * 2 + 1)) {
+                return 1;
+            } else {
+                return 0;
+            }
+        } else {
+            if (destTime[STOP] * (downExtra + 1) > stopCount * ((abs((int) downFloor - (int) currentFloor)) * 2 + 1)) {
+                return -1;
+            } else {
                 return 0;
             }
         }
     }
-    //已在边界停留
-    if (direction == 0) {
-        if (triggeredPassengers.size() == 0 && world.getElevator()->getInsidePassengers().size() == 0) {
-            return 0;
-        } else {
-            if (currentFloor == 1) {
-                direction = 1;
-                return 1;
-            } else {
-                direction = -1;
-                return -1;
-            }
-        }
-    }
 
 
-    return direction;
 }
